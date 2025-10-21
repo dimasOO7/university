5706e738c4a5b18b5e3377caa35c065f7ffd330f
dimas007 <dimas007>
2025-10-21 18:12:35 +0400

    доработка второй лабы по практикому по проге

--- programming_practics/labs/lab2/obj/Debug/net9.0/lab2.AssemblyInfo.cs
+++ programming_practics/labs/lab2/obj/Debug/net9.0/lab2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c7ea19f1accc3d010e40f53abf0175053c4dd9")]
-[assembly: System.Reflection.AssemblyProductAttribute("lab1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
+[assembly: System.Reflection.AssemblyProductAttribute("lab2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
