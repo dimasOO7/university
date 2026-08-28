061bab5e6491890bcfbb1b85f64d2b5a985daf21
dimas007 <dima.shturmavik@yandex.ru>
2026-08-28 19:26:30 +0400

    1 лаба по ооп 3 задание

--- oop/labWorks/labWork1/MyFirstProgram.java
+++ oop/labWorks/labWork1/MyFirstProgram.java
@@ -4,6 +4,9 @@ class MyFirstClass
 {
     public static void main(String[] args)
     {
-        System.out.println("Кузнецов");
+        for(int i = 0; i < args.length;i++)
+        {
+            System.out.println(args[i]);
+        }
     }
 }
